bb3d5cfa8a80f4cf321761ab61760ded74bef2d8
tkfj <tk@fjworks.jp>
2026-06-27 15:12:18 +0000

    新たにタグづけされた番組を区別可能にする処理で、過去タグを参照できていない問題を再修正

--- resources/views/programs/index.blade.php
+++ resources/views/programs/index.blade.php
@@ -201,7 +201,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         
                         <div class="flex flex-wrap items-center gap-2 text-xs">
                             <span class="px-2 py-0.5 border text-[11px] rounded font-mono {{ $get_badge_class($prog['interaction_next'] ?? $prog['interaction'] ?? '') }}">
-                                Interaction: {{ $prog['interaction_next'] ?? $prog['interaction'] }}{{ ($prog['interaction_next'] ?? '') == $prog['interaction'] ? '' : '*'}}
+                                Interaction: {{ $prog['interaction_next'] ?? $prog['interaction'] }}{{ ($prog['interaction_next'] ?? '_') == $prog['interaction'] ? '' : '*'}}
                             </span>
                             
                             <span class="px-2 py-0.5 border text-[11px] rounded font-mono {{ $get_badge_class($prog['pred_label'] ?? '') }}">
